d77b17f94c9b6af095db553aab149ed4049cf649
Sparsh Sharma <sparsh.sharma@mail.ru>
2023-11-06 14:13:57 +0100

    Added every nth eddy event prompt

--- plot2d.py
+++ plot2d.py
@@ -1,20 +1,19 @@
 import time
-
 import numpy as np
-
 import matplotlib.pyplot as pl
 
 start_time = time.time()
 
-
-lScale = 1.   #< ref. len. [m]
-tScale = 1.   #< ref. time [sec]
-domainLength = 2.  #< [m] -- see yaml
-xDomainCenter = 1.  #< [m] -- see yaml
-
+lScale = 1.   # ref. len. [m]
+tScale = 1.   # ref. time [sec]
+domainLength = 2.  # [m] -- see yaml
+xDomainCenter = 1.  # [m] -- see yaml
 
 # Prompt the user for the file location
 file_location = input("Enter the file location for the eddy sequence data: ")
+
+# Prompt the user for the value of n
+n = int(input("Enter the value of n for plotting every nth Eddy Event: "))
 
 # Load the eddy sequence data from the user-provided file location
 try:
@@ -23,33 +22,29 @@ except FileNotFoundError:
     print("File not found. Please make sure the file exists and the path is correct.")
     exit()
 
+eddySizes = eddySeq[:, 2-1] / lScale
+eddyLeftEdges = (eddySeq[:, 3-1] - (-0.5 * domainLength + xDomainCenter)) / lScale
+eddyRightEdges = (eddySeq[:, 4-1] - (-0.5 * domainLength + xDomainCenter)) / lScale
+eddyTimes = eddySeq[:, 10-1] / tScale
 
-eddySizes = eddySeq[:,2-1] / lScale
-eddyLeftEdges = (eddySeq[:,3-1] - (-0.5*domainLength + xDomainCenter)) / lScale   # channel: y in [0,2]
-eddyRightEdges = (eddySeq[:,4-1] - (-0.5*domainLength + xDomainCenter)) / lScale   # channel: y in [0,2]
-eddyTimes = eddySeq[:,10-1] / tScale
-# idx = np.where(eddyTimes > -99)  #1: uses all eddies
-idx = np.where((eddyTimes >= 0.) & (eddyTimes <= 1.0))  #2: truncate selection
-neddy = len(eddyTimes[idx])
-     
-pl.figure(1)   
-pl.figure(figsize=(10,6))
-# pl.figure(aspect=1)
+pl.figure(1)
+pl.figure(figsize=(10, 6))
 pl.xticks(fontsize=20)
 pl.yticks(fontsize=20)
-pl.xlabel('x (m)',fontsize=20)
-pl.ylabel('y (m)',labelpad=1.5, fontsize=20)
-pl.xlim(0,.5)
-pl.ylim(-0.05,0.05)
-for j in range(neddy):
+pl.xlabel('x (m)', fontsize=20)
+pl.ylabel('y (m)', labelpad=1.5, fontsize=20)
+pl.xlim(0, 0.5)
+pl.ylim(-0.05, 0.05)
+
+for j in range(0, len(eddyTimes), n):
     # Draw horizontal bars connecting the left and right edges
-    pl.hlines(eddyLeftEdges[idx][j], eddyTimes[idx][j] - 0.001, eddyTimes[idx][j] + 0.001, colors='k', lw=0.5)
-    pl.hlines(eddyRightEdges[idx][j], eddyTimes[idx][j] - 0.001, eddyTimes[idx][j] + 0.001, colors='k', lw=0.5)
+    pl.hlines(eddyLeftEdges[j], eddyTimes[j] - 0.001, eddyTimes[j] + 0.001, colors='k', lw=0.5)
+    pl.hlines(eddyRightEdges[j], eddyTimes[j] - 0.001, eddyTimes[j] + 0.001, colors='k', lw=0.5)
 
     # Plot vertical lines at the eddy times
-    pl.plot([eddyTimes[idx][j], eddyTimes[idx][j]], [eddyLeftEdges[idx][j], eddyRightEdges[idx][j]], 'k_-', lw=0.5, ms=0.3)
+    pl.plot([eddyTimes[j], eddyTimes[j]], [eddyLeftEdges[j], eddyRightEdges[j]], 'k_-', lw=0.5, ms=0.3)
 
-pl.savefig('eddySeq-.pdf')  
+# Uncomment this line to save the figure
+# pl.savefig('eddySeq-.pdf')
 
-print("--- %s seconds ---" % (time.time() - start_time))   
-     
+print("--- %s seconds ---" % (time.time() - start_time))
